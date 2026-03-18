570a60cf0c90f4b3d49252ff5c437ee6a032a367
gaponov <dgaponov99@gmail.com>
2026-03-18 14:59:19 +0600

    Основная логика my-bank-notifications

---
+++ my-bank-notifications/src/test/java/com/github/dgaponov99/practicum/mybank/notifications/MyBankNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dgaponov99.practicum.mybank.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBankNotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
