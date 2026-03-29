8f9f54be04cf36478e25fb753d8128942fd9068d
Dmitry Gaponov <dgaponov99@gmail.com>
2026-03-30 03:46:04 +0700

    CircuitBreaker для обращений в accounts-service в transfer-service и cash-service

--- my-bank-cash/src/main/java/com/github/dgaponov99/practicum/mybank/cash/service/CashService.java
+++ my-bank-cash/src/main/java/com/github/dgaponov99/practicum/mybank/cash/service/CashService.java
@@ -1,6 +1,6 @@
 package com.github.dgaponov99.practicum.mybank.cash.service;
 
-import com.github.dgaponov99.practicum.mybank.cash.client.AccountServiceClient;
+import com.github.dgaponov99.practicum.mybank.cash.gateway.AccountsGateway;
 import com.github.dgaponov99.practicum.mybank.cash.gateway.NotificationsGateway;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,16 +9,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CashService {
 
-    private final AccountServiceClient accountServiceClient;
+    private final AccountsGateway accountsGateway;
     private final NotificationsGateway notificationsGateway;
 
     public void withdraw(String username, int amount) {
-        accountServiceClient.debit(username, amount);
+        accountsGateway.debit(username, amount);
         notificationsGateway.sendNotification(username, "Успешно снята сумма %d руб.".formatted(amount));
     }
 
     public void deposit(String username, int amount) {
-        accountServiceClient.credit(username, amount);
+        accountsGateway.credit(username, amount);
         notificationsGateway.sendNotification(username, "Успешно внесена сумма %d руб.".formatted(amount));
     }
 
--- my-bank-transfer/src/main/java/com/github/dgaponov99/practicum/mybank/transfer/service/TransferService.java
+++ my-bank-transfer/src/main/java/com/github/dgaponov99/practicum/mybank/transfer/service/TransferService.java
@@ -1,8 +1,8 @@
 package com.github.dgaponov99.practicum.mybank.transfer.service;
 
-import com.github.dgaponov99.practicum.mybank.transfer.client.AccountServiceClient;
 import com.github.dgaponov99.practicum.mybank.transfer.dto.TransferAccountDto;
 import com.github.dgaponov99.practicum.mybank.transfer.dto.TransferDto;
+import com.github.dgaponov99.practicum.mybank.transfer.gateway.AccountsGateway;
 import com.github.dgaponov99.practicum.mybank.transfer.gateway.NotificationsGateway;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,18 +13,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TransferService {
 
-    private final AccountServiceClient accountServiceClient;
+    private final AccountsGateway accountsGateway;
     private final NotificationsGateway notificationsGateway;
 
     public List<TransferAccountDto> getTransferAccounts(String username) {
-        return accountServiceClient.getAllAccounts().stream()
+        return accountsGateway.getAllAccounts().stream()
                 .filter(account -> !username.equals(account.username()))
                 .map(account -> new TransferAccountDto(account.username(), account.name()))
                 .toList();
     }
 
     public void transfer(TransferDto transferDto) {
-        accountServiceClient.transfer(transferDto);
+        accountsGateway.transfer(transferDto);
         notificationsGateway.sendNotification(transferDto.fromUsername(), "Успешно выполнен исходящий перевод на сумму %d руб.".formatted(transferDto.amount()));
         notificationsGateway.sendNotification(transferDto.toUsername(), "Успешно выполнен входящий перевод на сумму %d руб.".formatted(transferDto.amount()));
     }
