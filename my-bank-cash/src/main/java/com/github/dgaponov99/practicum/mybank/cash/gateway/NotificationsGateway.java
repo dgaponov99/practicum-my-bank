3e6964f625e3d5f4c214e694d7a7cd4972234810
Dmitry Gaponov <dgaponov99@gmail.com>
2026-04-07 06:26:17 +0700

    Отправка уведомлений через Kafka в transfer-service и chat-service

--- my-bank-cash/src/main/java/com/github/dgaponov99/practicum/mybank/cash/gateway/NotificationsGateway.java
+++ my-bank-cash/src/main/java/com/github/dgaponov99/practicum/mybank/cash/gateway/NotificationsGateway.java
@@ -1,15 +1,12 @@
 package com.github.dgaponov99.practicum.mybank.cash.gateway;
 
-import com.github.dgaponov99.practicum.mybank.cash.client.NotificationsClient;
 import com.github.dgaponov99.practicum.mybank.cash.dto.NotificationDto;
 import com.github.dgaponov99.practicum.mybank.cash.persistence.entity.NotificationOutbox;
 import com.github.dgaponov99.practicum.mybank.cash.persistence.repository.NotificationOutboxRepository;
 import com.github.dgaponov99.practicum.mybank.cash.properties.OutboxProperties;
-import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
-import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.data.domain.Pageable;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -19,29 +16,22 @@ import java.time.Instant;
 @RequiredArgsConstructor
 public class NotificationsGateway {
 
-    private final NotificationsClient notificationsClient;
     private final NotificationOutboxRepository notificationOutboxRepository;
     private final OutboxProperties outboxProperties;
-    private final CircuitBreakerFactory<?, ?> breakerFactory;
-    private final CircuitBreakerRegistry registry;
+    private final KafkaTemplate<String, NotificationDto> kafkaTemplate;
 
     public void sendNotification(String username, String message) {
         var notificationDto = new NotificationDto(username, message);
-        getCircuitBreaker()
-                .run(() -> {
-                    notificationsClient.sendNotification(notificationDto);
-                    return null;
-                }, (e) -> {
-                    saveOutbox(notificationDto);
-                    return null;
+        kafkaTemplate.send("notifications", notificationDto.username(), notificationDto)
+                .whenComplete((notification, throwable) -> {
+                    if (throwable != null) {
+                        saveOutbox(notificationDto);
+                    }
                 });
     }
 
     @Scheduled(fixedDelay = 5000)
     protected void outboxSchedule() {
-        if (isOpen()) {
-            return;
-        }
 
         var notifications = notificationOutboxRepository.findReady(Instant.now(), Pageable.ofSize(outboxProperties.getBatchSize()));
         if (notifications.isEmpty()) {
@@ -49,16 +39,14 @@ public class NotificationsGateway {
         }
 
         notifications.forEach(notification -> {
-            getCircuitBreaker()
-                    .run(() -> {
-                        notificationsClient.sendNotification(notification.getPayload());
-                        notificationOutboxRepository.delete(notification);
-                        return null;
-                    }, (e) -> {
-                        notification.setRetryCount(notification.getRetryCount() + 1);
-                        notification.setNextRetryAt(getNextRetryAt(notification.getRetryCount()));
-                        notificationOutboxRepository.save(notification);
-                        return null;
+            var notificationDto = notification.getPayload();
+            kafkaTemplate.send("notifications", notificationDto.username(), notificationDto)
+                    .whenComplete((result, e) -> {
+                        if (e != null) {
+                            notification.setRetryCount(notification.getRetryCount() + 1);
+                            notification.setNextRetryAt(getNextRetryAt(notification.getRetryCount()));
+                            notificationOutboxRepository.save(notification);
+                        }
                     });
         });
     }
@@ -76,14 +64,6 @@ public class NotificationsGateway {
         var delay = (long) Math.pow(2, retryCount + 1) * outboxProperties.getBaseDelay();
         delay = Math.min(delay, outboxProperties.getMaxDelay());
         return Instant.now().plusSeconds(delay);
-    }
-
-    private CircuitBreaker getCircuitBreaker() {
-        return breakerFactory.create("notifications-service");
-    }
-
-    private boolean isOpen() {
-        return registry.circuitBreaker("notifications-service").getState() == io.github.resilience4j.circuitbreaker.CircuitBreaker.State.OPEN;
     }
 
 }
